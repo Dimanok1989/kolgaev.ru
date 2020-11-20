14f7b30e38a5f9dd9423eb0d9a4283b6940e73df
Dimanok1989 <kolgaev@gmail.com>
2020-11-20 22:32:26 +0300

    User registration

--- src/Components/Header/Login.jsx
+++ src/Components/Header/Login.jsx
@@ -20,16 +20,35 @@ class Login extends React.Component {
             auth: false,
             registration: false,
             error: null,
+            valid: {
+                email: false,
+                password: false,
+                password_confirmation: false,
+                surname: false,
+                name: false,
+                patronymic: false,
+            }
         }
 
     }
 
-    auth = () => {
+    authOpen = () => {
 
         this.setState({
             auth: true,
             process: false,
             registration: false,
+            error: null,
+        });
+
+    }
+
+    registrationOpen = () => {
+
+        this.setState({
+            auth: false,
+            process: false,
+            registration: true,
             error: null,
         });
 
@@ -66,9 +85,54 @@ class Login extends React.Component {
 
     }
 
+    registration = () => {
+
+        this.setState({ process: true });
+
+        let form = document.getElementById('registration-form');
+        let formdata = new FormData(form);
+
+        axios.post('auth/registration', formdata).then(({ data }) => {
+
+            // Запись куков на все поддомены
+            Cookies.set('token', data.token, {
+                expires: 365,
+                domain: '.' + window.location.host,
+            });
+
+            localStorage.setItem('token', data.token);
+            window.user = data.user;
+
+            this.props.logined();
+
+        }).catch(error => {
+
+            let errors = error.response.data.errors || null,
+                valid = this.state.valid;
+
+            for (let index in valid)
+                valid[index] = false;
+
+            if (typeof errors == "object") {
+                for (let index in errors)
+                    valid[index] = errors[index].toString();
+            }
+
+            this.setState({
+                valid,
+                process: false
+            });
+
+        });
+
+    }
+
     render() {
 
-        const error = this.state.error ? <div className="text-danger mt-2 font-weight-bold">{this.state.error}</div> : null;
+        let error = null;
+
+        if (this.state.error && this.state.auth)
+            error = <div className="text-danger mt-2 font-weight-bold">{this.state.error}</div>;
 
         const auth = <div>
             <Modal
@@ -76,6 +140,7 @@ class Login extends React.Component {
                 show={this.state.auth}
                 onHide={() => this.setState({ auth: false })}
                 centered
+                backdrop="static"
             >
 
                 <Modal.Header closeButton className="border-bottom-0 pb-0">
@@ -102,13 +167,96 @@ class Login extends React.Component {
             </Modal>
         </div>
 
+        const valid = this.state.valid;
+
+        const registration = <div>
+            <Modal
+                size="sm"
+                show={this.state.registration}
+                onHide={() => this.setState({ registration: false })}
+                centered
+                backdrop="static"
+            >
+
+                <Modal.Header closeButton className="border-bottom-0 pb-0">
+                    <Modal.Title>Регистрация</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+
+                    <Dimmer active={this.state.process} className="rounded-bottom" inverted>
+                        <Loader />
+                    </Dimmer>
+
+                    <form id="registration-form">
+
+                        <label className="mb-0 px-1">Электронная почта</label>
+                        <Input
+                            placeholder="Введите адрес почты *"
+                            className="w-100 mt-1"
+                            name="email"
+                            onKeyUp={e => e.keyCode === 13 ? this.registration() : null}
+                            error={valid.email ? true : false}
+                        />
+                        {valid.email ? <small className="text-danger valid-message">{valid.email}</small> : ''}
+
+                        <label className="mt-2 mb-0 px-1">Пароль</label>
+                        <Input
+                            placeholder="Введите пароль *"
+                            className="w-100 mt-1"
+                            type="password"
+                            name="password"
+                            onKeyUp={e => e.keyCode === 13 ? this.registration() : null}
+                            error={valid.password ? true : false}
+                        />
+                        {valid.password ? <small className="text-danger valid-message">{valid.password}</small> : ''}
+                        <Input
+                            placeholder="Подтвердите пароль *"
+                            className="w-100 mt-1"
+                            type="password"
+                            name="password_confirmation"
+                            onKeyUp={e => e.keyCode === 13 ? this.registration() : null}
+                            error={valid.password_confirmation ? true : false}
+                        />
+                        {valid.password_confirmation ? <small className="text-danger valid-message">{valid.password_confirmation}</small> : ''}
+
+                        <label className="mt-2 mb-0 px-1">Представьтесь</label>
+                        <Input
+                            placeholder="Ваше имя *"
+                            className="w-100 mt-1"
+                            name="name"
+                            onKeyUp={e => e.keyCode === 13 ? this.registration() : null}
+                            error={valid.name ? true : false}
+                        />
+                        {valid.name ? <small className="text-danger valid-message">{valid.name}</small> : ''}
+                        <Input
+                            placeholder="Ваша фамилия"
+                            className="w-100 mt-1"
+                            name="surname"
+                            onKeyUp={e => e.keyCode === 13 ? this.registration() : null}
+                        />
+
+                    </form>
+
+                    <Button variant="success" className="mt-3 btn-block" onClick={this.registration}>Зарегистрироваться</Button>
+
+                    {error}
+
+                </Modal.Body>
+
+            </Modal>
+        </div>
+
         const avatar = <span className="avatar d-flex align-items-center justify-content-center">
             <Icon icon={['fas', 'user']} />
         </span>
 
         return (
             <>
+
                 {auth}
+                {registration}
+
                 <div className="position-absolute mr-3 logined-header cursor-pointer">
 
                     <Dimmer active={this.state.loading} className="loading-header-dimmer">
@@ -118,13 +266,14 @@ class Login extends React.Component {
                     <Dropdown trigger={avatar} icon={null} pointing='top right'>
 
                         <Dropdown.Menu>
-                            <Dropdown.Item icon="sign in" text='Авторизация' onClick={this.auth} />
-                            <Dropdown.Item icon="user plus" text='Регистрация' />
+                            <Dropdown.Item icon="user plus" text='Регистрация' onClick={this.registrationOpen} />
+                            <Dropdown.Item icon="sign in" text='Вход' onClick={this.authOpen} />
                         </Dropdown.Menu>
 
                     </Dropdown>
 
                 </div>
+
             </>
         )
 
